52593e389364d7018c965bd330107f7c7a60a7c7
JeffreyBull76 <jeffreybull@sky.com>
2022-09-12 15:40:46 +0000

    add basic randomiser and deck creation code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -30,3 +30,34 @@ function myFunction (event) {
 
 let myButton = document.getElementById('next-card');
 myButton.addEventListener('click', myFunction);
+
+/**
+ * Below is some rudimentary code to create two arrays based on a randomised shuffle or the main arrary 
+ * link to splice method
+ * https://thewebdev.info/2022/06/26/how-to-split-array-into-two-arrays-with-javascript/#:~:text=To%20split%20array%20into%20two%20arrays%20with%20JavaScript%2C%20we%20can,indexToSplit)%3B%20const%20second%20%3D%20arr.
+ * links to randomiser
+ * https://javascript.info/task/shuffle (shuffle array)
+ */
+
+let myTestArray = ['1', '2', '3', '4', '5', '6'];
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+  
+      // swap elements array[i] and array[j]
+      // we use "destructuring assignment" syntax to achieve that
+      // you'll find more details about that syntax in later chapters
+      // same can be written as:
+      // let t = array[i]; array[i] = array[j]; array[j] = t
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+};
+
+shuffle(myTestArray);  
+console.log(myTestArray);
+
+let playerDeck = myTestArray.slice(0, 3)
+let computerDeck = myTestArray.slice(3, 6)
+console.log(playerDeck);
+console.log(computerDeck);

54c0c0aad31de76328dd13da4313193f3ce8b1e8
JeffreyBull76 <jeffreybull@sky.com>
2022-09-12 14:02:16 +0000

    base code to allow text of card change

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+const card = [{
+    name: 'Dracula',
+    image: `<img src="assets/images/Test_Img2.png" alt="" srcset="">`,
+    kpower: 85,
+    ffactor: 75,
+    hrating: 96,
+    pstrength: 85,
+},
+{
+    name: 'Michael Myers',
+    image: `<img src="assets/images/Test_Img2.png" alt="" srcset="">`,
+    kpower: 75,
+    ffactor: 90,
+    hrating: 90,
+    pstrength: 90,
+}
+];
+
+function myFunction (event) {
+    
+  }
+
+console.log(card[0].kpower);
+
+let myButton = document.getElementById('next-card');
+myButton.addEventListener('click', myFunction);

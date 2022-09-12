39c1927d684998375f3457b6fabd2c5723727f48
JeffreyBull76 <jeffreybull@sky.com>
2022-09-12 14:44:50 +0000

    very basic randomiser to test loading card details

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,26 +1,32 @@
 const card = [{
     name: 'Dracula',
-    image: `<img src="assets/images/Test_Img2.png" alt="" srcset="">`,
-    kpower: 85,
-    ffactor: 75,
-    hrating: 96,
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 84,
+    ffactor: 77,
+    hrating: 97,
     pstrength: 85,
 },
 {
     name: 'Michael Myers',
     image: `<img src="assets/images/Test_Img2.png" alt="" srcset="">`,
-    kpower: 75,
-    ffactor: 90,
-    hrating: 90,
-    pstrength: 90,
+    kpower: 76,
+    ffactor: 88,
+    hrating: 92,
+    pstrength: 93,
 }
 ];
 
 function myFunction (event) {
-    
-  }
+    let rnum = Math.round(Math.random());
+    document.getElementById('name').textContent = card[rnum].name;
+    document.getElementById('card-area').children[3].outerHTML = card[rnum].image;
+    document.getElementById('kpower').innerText = card[rnum].kpower;
+    document.getElementById('ffactor').innerText = card[rnum].ffactor;
+    document.getElementById('hrating').innerText = card[rnum].hrating;
+    document.getElementById('pstrength').innerText = card[rnum].pstrength;
+}
 
-console.log(card[0].kpower);
+
 
 let myButton = document.getElementById('next-card');
 myButton.addEventListener('click', myFunction);

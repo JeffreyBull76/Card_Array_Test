bb28656c7335ad0938b4a11e0917bb050f2934d4
JeffreyBull76 <jeffreybull@sky.com>
2022-09-17 20:37:14 +0000

    add code to hide computer fields

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,50 +1,314 @@
 const card = [{
     name: 'Dracula',
     image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
-    kpower: 84,
-    ffactor: 77,
-    hrating: 97,
-    pstrength: 85,
+    kpower: 88,
+    ffactor: 78,
+    hrating: 98,
+    pstrength: 84,
+},
+{
+    name: 'Jason Vorhees',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 94,
+    ffactor: 83,
+    hrating: 88,
+    pstrength: 94,
 },
 {
     name: 'Michael Myers',
-    image: `<img src="assets/images/Test_Img2.png" alt="" srcset="">`,
-    kpower: 76,
-    ffactor: 88,
-    hrating: 92,
-    pstrength: 93,
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 96,
+    ffactor: 89,
+    hrating: 94,
+    pstrength: 91,
 },
 {
     name: 'Freddy Krueger',
     image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
-    kpower: 94,
-    ffactor: 87,
-    hrating: 70,
-    pstrength: 75,
+    kpower: 97,
+    ffactor: 75,
+    hrating: 95,
+    pstrength: 82,
 },
 {
-    name: 'Jason Vorhees',
-    image: `<img src="assets/images/Test_Img2.png" alt="" srcset="">`,
-    kpower: 82,
-    ffactor: 82,
+    name: 'Candyman',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 93,
+    ffactor: 91,
+    hrating: 85,
+    pstrength: 83,
+},
+{
+    name: 'Chucky',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 73,
+    ffactor: 65,
+    hrating: 71,
+    pstrength: 76,
+},
+{
+    name: 'Leprechaun',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 79,
+    ffactor: 62,
+    hrating: 64,
+    pstrength: 58,
+},
+{
+    name: 'Alien',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 97,
+    ffactor: 77,
+    hrating: 91,
+    pstrength: 81,
+},
+{
+    name: 'Scanners',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 86,
+    ffactor: 64,
+    hrating: 62,
+    pstrength: 71,
+},
+{
+    name: 'The Thing',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 99,
+    ffactor: 91,
     hrating: 83,
-    pstrength: 98,
+    pstrength: 82,
+},
+{
+    name: 'Jack Torance',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 74,
+    ffactor: 91,
+    hrating: 95,
+    pstrength: 69,
+},
+{
+    name: 'Wolfman',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 75,
+    ffactor: 72,
+    hrating: 97,
+    pstrength: 85,
+},
+{
+    name: 'Frankenstein',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 68,
+    ffactor: 73,
+    hrating: 95,
+    pstrength: 96,
+},
+{
+    name: 'Evil Dead',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 93,
+    ffactor: 84,
+    hrating: 83,
+    pstrength: 72,
+},
+{
+    name: 'The Fly',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 81,
+    ffactor: 87,
+    hrating: 77,
+    pstrength: 91,
+},
+{
+    name: 'Nosferatu',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 78,
+    ffactor: 89,
+    hrating: 91,
+    pstrength: 76,
+},
+{
+    name: 'Pazuzu',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 64,
+    ffactor: 88,
+    hrating: 90,
+    pstrength: 59,
+},
+{
+    name: 'Angela',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 78,
+    ffactor: 74,
+    hrating: 70,
+    pstrength: 82,
+},
+{
+    name: 'Argentos Demons',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 75,
+    ffactor: 80,
+    hrating: 74,
+    pstrength: 86,
+},
+{
+    name: 'Zombie Flesh Eaters',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 83,
+    ffactor: 86,
+    hrating: 77,
+    pstrength: 82,
 },
 {
     name: 'Pinhead',
     image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
-    kpower: 68,
-    ffactor: 90,
+    kpower: 85,
+    ffactor: 87,
     hrating: 92,
+    pstrength: 70,
+},
+{
+    name: 'Leatherface',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 74,
+    ffactor: 86,
+    hrating: 92,
+    pstrength: 92,
+},
+{
+    name: 'Tetsuo',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 72,
+    ffactor: 64,
+    hrating: 79,
     pstrength: 74,
 },
 {
-    name: 'Romero Zombie',
-    image: `<img src="assets/images/Test_Img2.png" alt="" srcset="">`,
-    kpower: 63,
-    ffactor: 83,
-    hrating: 93,
+    name: 'Sadako',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 79,
+    ffactor: 90,
+    hrating: 89,
+    pstrength: 66,
+},
+{
+    name: 'Belial',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 61,
+    ffactor: 64,
+    hrating: 86,
+    pstrength: 76,
+},
+{
+    name: 'The Red Death',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 76,
+    ffactor: 72,
+    hrating: 82,
+    pstrength: 64,
+},
+{
+    name: 'Dr Phibes',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 82,
+    ffactor: 79,
+    hrating: 66,
+    pstrength: 62,
+},
+{
+    name: 'Hannibal Lecter',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 87,
+    ffactor: 73,
+    hrating: 76,
+    pstrength: 62,
+},
+{
+    name: 'Pumpkinhead',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 77,
+    ffactor: 68,
+    hrating: 59,
     pstrength: 83,
+},
+{
+    name: 'The Blob',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 83,
+    ffactor: 74,
+    hrating: 67,
+    pstrength: 81,
+},
+{
+    name: 'Hitcher',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 82,
+    ffactor: 66,
+    hrating: 79,
+    pstrength: 60,
+},
+{
+    name: 'Society',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 71,
+    ffactor: 72,
+    hrating: 61,
+    pstrength: 64,
+},
+{
+    name: 'Blair Witch',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 65,
+    ffactor: 87,
+    hrating: 83,
+    pstrength: 69,
+},
+{
+    name: 'Pennywise',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 83,
+    ffactor: 91,
+    hrating: 90,
+    pstrength: 85,
+},
+{
+    name: 'Body Snatchers',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 76,
+    ffactor: 81,
+    hrating: 78,
+    pstrength: 90,
+},
+{
+    name: 'Wish Master',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 80,
+    ffactor: 78,
+    hrating: 68,
+    pstrength: 72,
+},
+{
+    name: 'Driller Killer',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 77,
+    ffactor: 72,
+    hrating: 83,
+    pstrength: 63,
+},
+{
+    name: 'Us',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 81,
+    ffactor: 77,
+    hrating: 88,
+    pstrength: 67,
+},
+{
+    name: 'Jigsaw',
+    image: `<img src="assets/images/Test_Img1.png" alt="" srcset="">`,
+    kpower: 83,
+    ffactor: 68,
+    hrating: 79,
+    pstrength: 72,
 },
 ];
 
@@ -74,8 +338,8 @@ const computerWins =[];
 
 shuffle(card);  
 
-let playerDeck = card.slice(0, 3)
-let computerDeck = card.slice(3, 6)
+let playerDeck = card.slice(0, 20)
+let computerDeck = card.slice(20, 40)
 
 function playCards (event) {
     let activePlayerCard = playerDeck[0];
@@ -106,12 +370,18 @@ function playCards (event) {
         document.getElementById('pstrength1').innerText = activePlayerCard.pstrength;
     
         document.getElementById('name2').innerText = activeComputerCard.name;
+        document.getElementById('name2').hidden = true;
         document.getElementById('card-area2').children[3].outerHTML = activeComputerCard.image;
+        document.getElementById('card-area2').children[3].style.visibility = "hidden";
         document.getElementById('kpower2').innerText = activeComputerCard.kpower;
+        document.getElementById('kpower2').hidden = true;
         document.getElementById('ffactor2').innerText = activeComputerCard.ffactor;
+        document.getElementById('ffactor2').hidden = true;
         document.getElementById('hrating2').innerText = activeComputerCard.hrating;
+        document.getElementById('hrating2').hidden = true;
         document.getElementById('pstrength2').innerText = activeComputerCard.pstrength;
-    
+        document.getElementById('pstrength2').hidden = true;
+        
     }
 
 };
@@ -136,7 +406,13 @@ let fieldC4 = document.getElementById('pstrength2');
 function setKpowerField() {
     fieldP = fieldP1.innerHTML;
     fieldC = fieldC1.innerHTML;
-    battle();
+    document.getElementById('name2').hidden = false;
+    document.getElementById('card-area2').children[3].style.visibility = "visible";
+    document.getElementById('kpower2').hidden = false;
+    document.getElementById('ffactor2').hidden = false;
+    document.getElementById('hrating2').hidden = false;
+    document.getElementById('pstrength2').hidden = false;
+    setTimeout(battle, 1200);
 };
 
 function setFfactorField() {
